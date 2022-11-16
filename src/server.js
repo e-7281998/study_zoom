7365848874caddaa1cbe9505f18516dc84464b71
e-7281998 <jejjcs43@naver.com>
2022-11-16 15:58:01 +0900

    recap

--- src/server.js
+++ src/server.js
@@ -22,14 +22,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-    // socket.on("enter_room", (msg) => console.log(msg));
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
+    socket.on("enter_room", (roomName, done) => {
+        console.log(roomName);
         setTimeout(() => {
-            done();
-        })
-    }, 10000);
-
+            done("hello from backend");
+        }, 10000);
+    });
 });
 
 //---webSocket 서버
